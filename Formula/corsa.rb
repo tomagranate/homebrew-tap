3e111b49c9ca26bc131e578d33c0692a0eb3d796
Tom Grant <tom@sunburst.io>
2026-01-31 01:42:30 -0500

    refactor: streamline installation logic for ARM and Intel architectures in Corsa formula

--- Formula/corsa.rb
+++ Formula/corsa.rb
@@ -5,26 +5,46 @@ class Corsa < Formula
   license "MIT"
 
   on_macos do
-    if Hardware::CPU.arm?
+    on_arm do
       url "https://github.com/tomagranate/corsa/releases/download/v#{version}/corsa-darwin-arm64.tar.gz"
       sha256 "bbfa7dc4c9ca04efe8ee05c46462aad8ee4edeb2b8349eebfda63ed8f32ccffb"
-    else
+
+      def install
+        bin.install "corsa-darwin-arm64" => "corsa"
+      end
+    end
+
+    on_intel do
       url "https://github.com/tomagranate/corsa/releases/download/v#{version}/corsa-darwin-x64.tar.gz"
       sha256 "41d24dade4d08356a33c5d9a080faf464c13acb50700e6f2f208db78c9881bba"
+
+      def install
+        bin.install "corsa-darwin-x64" => "corsa"
+      end
     end
   end
 
   on_linux do
-    if Hardware::CPU.arm?
+    on_arm do
       url "https://github.com/tomagranate/corsa/releases/download/v#{version}/corsa-linux-arm64.tar.gz"
       sha256 "0867cae205d3730e63efda86cc9154fb4f036d91929159a6ee8e1f0c5c20553f"
-    else
+
+      def install
+        bin.install "corsa-linux-arm64" => "corsa"
+      end
+    end
+
+    on_intel do
       url "https://github.com/tomagranate/corsa/releases/download/v#{version}/corsa-linux-x64.tar.gz"
       sha256 "1c68500ec383d2e86c502abae0bf4d434e06341a2c73e689336e9d1756f1cb48"
+
+      def install
+        bin.install "corsa-linux-x64" => "corsa"
+      end
     end
   end
 
-  def install
-    bin.install "corsa"
+  test do
+    assert_match "corsa", shell_output("#{bin}/corsa --help")
   end
 end
